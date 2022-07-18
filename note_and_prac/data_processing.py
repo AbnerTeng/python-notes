44b7fe7451e30ac799085e6a977435f43d5fabde
AbnerTeng <abnerteng16@gmail.com>
2022-07-18 13:42:02 +0800

    0718

--- note_and_prac/data_processing.py
+++ note_and_prac/data_processing.py
@@ -5,6 +5,7 @@
 
 # %%
 from cmath import nan
+import py_compile
 from more_itertools import last
 import pandas as pd
 import numpy as np
@@ -100,4 +101,95 @@ data5.rename(index={'OHIO': 'INDIANA'},
             columns={'three': 'THREE'})
 data5.rename(index={'OHIO': 'INDIANA'}, inplace=True)
 print(data5)
+
+# Discretization and Binning
+# When analyzing data, we should deicretization the data or put them into different groups.
+ages = [20, 22, 25, 27, 21, 23, 37, 31, 61, 45, 41, 32]
+
+# We want to hve groups of 18-25, 26-35, 36-60 and over 61
+bins = [18, 25, 35, 60, 100] # (18, 25], (25, 35], ...
+cats = pd.cut(ages, bins)
+print(cats)
+print(cats.categories)
+print('amount of every groups\n', pd.value_counts(cats))
+
+# Customize lables
+group_names = ['Youth', 'YoungAdult', 'MiddleAged', 'Senior']
+pd.cut(ages, bins, labels=group_names)
+
+# What if we want to divide data by the amount of data
+data6 = np.random.rand(20) # uniform (0,1)
+pd.cut(data, 4, precision = 2) # Divide data into 4 parts, precision = 2 means 小數點第二位
+
+# quartile cut (qcut)
+data7 = np.random.randn(1000)
+cats2 = pd.qcut(data7, 4)
+print(cats2)
+
+# %% [markdown]
+# ## Find outliers, sample, dummies
+
+# %%
+# find and filt outliers
+data8 = pd.DataFrame(np.random.randn(1000, 4))
+print(f'statistical analys\n', data8.describe())
+column = data8[2]
+column[np.abs(column) > 3] # find outliers
+data8[(np.abs(data8) > 3).any(1)] # find every outliers
+
+# We can also make the data are always between -3 and 3
+data8[np.abs(data8) > 3] = np.sign(data8) * 3 # np.sign() will generate 1 or -1
+print(data8.describe())
+
+# Permutation and random sampling
+df2 = pd.DataFrame(np.arange(20).reshape((5, 4)))
+sampler = np.random.permutation(5) # np.random.permutation(length of axis)
+print(sampler)
+print(df2.take(sampler))
+
+# Dummy Variables
+df3 = pd.DataFrame({'key': ['b', 'b', 'a', 'c', 'a', 'b'],
+                   'data9': range(6)})
+pd.get_dummies(df3['key'])
+
+dummies = pd.get_dummies(df3['key'], prefix = 'key') # prefix is 字首
+df_with_dummy = df3[['data9']].join(dummies)
+print(df_with_dummy)
+
+movie_names = ['movie_id', 'title', 'genres']
+movies = pd.read_table('/Users/abnerteng/GitHub/pydata-book/datasets/movielens/movies.dat', sep = '::',
+                       header = None, names = movie_names)
+print(movies[:10])
+
+# pick out genres
+all_genres = []
+for x in movies.genres:
+  all_genres.extend(x.split('|'))
+genres = pd.unique(all_genres)
+print(genres)
+
+# one method to construct a DataFrame is to fill the 0-matrix
+zero_matrix = np.zeros((len(movies), len(genres)))
+dummies = pd.DataFrame(zero_matrix, columns = genres)
+
+#classify index value
+gen = movies.genres[0]
+gen.split('|')
+dummies.columns.get_indexer(gen.split('|'))
+
+for i, gen in enumerate(movies.genres):
+  indices = dummies.columns.get_indexer(gen.split('|'))
+  dummies.iloc[i, indices] = 1
+
+movies_windic = movies.join(dummies.add_prefix('Genre_'))
+print(movies_windic.iloc[0])
+
+# applied in statistic
+np.random.seed(123)
+values = np.random.rand(100)
+print(values)
+bins = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+print(pd.get_dummies(pd.cut(values, bins)))
+
+
 # %%
